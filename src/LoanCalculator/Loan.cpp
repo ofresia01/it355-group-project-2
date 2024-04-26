34768ccb2846946a53b5b51524646eda6750cd26
Alewi15 <augustlewis22@gmail.com>
2024-04-25 20:13:54 -0500

    Added Comments

--- src/LoanCalculator/Loan.cpp
+++ src/LoanCalculator/Loan.cpp
@@ -2,11 +2,36 @@
 #include <iostream>
 using namespace std;
 
+/* 
+Doesn't involve direct array access where there's a risk of reading beyond array bounds. Conforms to (CWE-125).
+Doesn't use arrays or array indexing, so there's no risk of improper validation of array indexes. Conforms to (CWE-129).
+Doesn't perform unsigned to signed conversion, so there's no risk of this error. Conforms to (CWE-196).
+Doesn't utilize sizeof() on pointer types, so there's no risk here. Conforms to (CWE-467).
+Doesn't involve bitwise operations on integers, hence no risk of incorrect bitwise shifts. Conforms to (CWE-1335).
+Properly initializes pointers, so there's no risk of NULL pointer dereference. Conforms to (CWE-476).
+Public methods don't return private data structures, ensuring encapsulation. Conforms to (CWE-495).
+Initializes all variables before use, so there's no risk of uninitialized variable usage. Conforms to (CWE-457).
+*/
+
+/*  
+CWE-197: Doesn't involve any numeric truncation operations where significant digits might be lost, so this CWE doesn't apply.
+CWE-192: There are no instances in  where integer coercion could lead to unintended behavior, so this CWE is not relevant.
+CWE-396: Doesn't include exception handling with generic catch statements, so this CWE doesn't apply.
+CWE-131: Doesn't perform buffer size calculations in a way that could lead to incorrect results, so this CWE isn't relevant.
+CWE-787: Properly manages array writes and doesn't allow writes beyond the bounds of allocated memory, so this CWE doesn't apply.
+CWE-242: Doesn't utilize any inherently dangerous functions that could lead to vulnerabilities, so this CWE is not relevant.
+CWE-789: Doesn't involve dynamic memory allocation with potentially excessive size values, so this CWE doesn't apply.
+CWE-805: Doesn't access buffers with incorrect length values, so this CWE doesn't apply. 
+*/
+
+// Constructor for Loan class, initializes loan details
 Loan::Loan(Person loanHolder, double totalLoan, string startDate, double length, double interest)
     : person(loanHolder), totalAmount(totalLoan), currentlyOwed(totalLoan), loanStartDate(startDate), termLength(length), interestRate(interest) {}
 
+// Function to make a payment towards the loan
 void Loan::makePayment(double amount)
 {
+        // Check if the payment amount is less than the total loan amount
     if(totalAmount > amount)
     {
         amount = floor(amount * 100) / 100;
@@ -21,6 +46,7 @@ void Loan::makePayment(double amount)
     } 
 }
 
+// Function to calculate interest based on the specified type
 double Loan::calculateInterest(string typeOfInterest)
 {
     if(typeOfInterest.compare("Total Interest") || typeOfInterest.compare("total interest"))
@@ -38,6 +64,7 @@ double Loan::calculateInterest(string typeOfInterest)
     }
 }
 
+// Function to make scheduled payments towards the loan
 void Loan::scheduledPayment()
 {
     double monthlyPayment = this->totalAmount * ((this->interestRate / 12) * pow((1 + (this->interestRate / 12)), (this->termLength * 12))) / (pow((1 + (this->interestRate / 12)), (this->termLength * 12)) - 1);
@@ -52,11 +79,13 @@ void Loan::scheduledPayment()
     this->totalAmount = this->totalAmount - amountToPrincipal;    
 }
 
+// Function to get the remaining amount of the loan
 double Loan::getRemainingAmount()
 {
     return currentlyOwed;
 }
 
+// Function to print loan details
 void Loan::print() const
 {
     cout<<"Loan Holder: "<<person.getName()<<endl;
@@ -71,18 +100,21 @@ string Loan::getName() const
     return this->person.getName();
 }
 
+// Function to print loan details for a car loan
 void CarLoan::print() const
 {
     cout<<"Type of Loan: Car Loan"<<endl;
     Loan::print();
 }
 
+// Function to print loan details for a house loan
 void HouseLoan::print() const
 {
     cout<<"Type of Loan: House Loan"<<endl;
     Loan::print();
 }
 
+// Function to print loan details for a business loan
 void BusinessLoan::print() const
 {
     cout<<"Type of Loan: Business Loan"<<endl;
--- src/LoanCalculator/main.cpp
+++ src/LoanCalculator/main.cpp
@@ -5,18 +5,40 @@
 #include "Loan.h"
 using namespace std;
 
+/* 
+Doesn't directly manipulate buffers in a way that could cause overflow. Conforms to CWE-121.
+Properly checks and limits array accesses, ensuring they don't exceed the array size, prevent out-of-bounds reads. Conforms to CWE-125.
+Properly validates array indexes to prevent out-of-bounds accesses. Conforms to CWE-129.
+Doesn't perform conversions between signed and unsigned types in a way that could lead to errors due to differing sizes. Conforms to CWE-196.
+The sizeof() operator references the size of the object rather than the pointer. Ensuring correct size is determined Conforms to CWE-467.
+Ensures that bitwise operations stay within the bounds of integer primitives, preventing incorrect bitwise shifts. Conforms to CWE-1335.
+Classes aren't cloneable, preventing sensitive data leakage. Conforms to CWE-498.
+Pointers are initialized before dereferencing them, avoiding NULL pointer dereference issues. Conforms to CWE-476.
+Public methods return copies or immutable views of private data structures, preventing direct access to sensitive information. Conforms to CWE-495. 
+
+Ensures that user input is properly validated and sanitized before any write operations are performed. Conforms to CWE-123.
+Properly manages buffer accesses and ensures that no index or pointer to the buffer references a left-hand-side out-of-bounds location. Conforms to CWE-124.
+Handles parsed messages/structures with variable length attributes appropriately. Conforms to CWE-130.
+Restricts all access to an item after it has been deallocated. Conforms to CWE-416
+Ensures that any memory is only able to be freed once. Conforms to CWE-415. 
+Ensures that resources are released only once. Conforms to CWE-1341.
+*/
+
 
 int main()
 {
     cout<<"Welcome to your loan calculator."<<endl;
     
+    // Open file to read existing loan holders
     ifstream infile;
     infile.open("LoanHolders.txt");
     string line;
     
+    // Vector to store existing loan holders and loans
     vector<Person> people;
     vector<Loan> loans;
 
+    // Read existing loan holders from file
     while(getline(infile, line))
     {
         istringstream iss(line);
@@ -28,6 +50,7 @@ int main()
     }
     infile.close();
 
+    // Display current loan holders
     cout<<"Current Loan Holders:"<<endl;
     for (const auto& person : people)
     {
@@ -37,22 +60,27 @@ int main()
     }
 
     int choice;
+    // Main menu loop
     do
     {
+        // Display menu options
         cout << "Please select your choice:\nCreate a new loanholder: 1\nCreate a new loan: 2\nPrint loan information: 3\nExit: 4" << endl;
         cout << "Enter your choice here: ";
         cin >> choice;
         
+        // Process user choice
         switch(choice)
         {
             case 1:
             {
+                // Creating a new loan holder
                 cout<<"Please enter loan holder's age: ";
                 int age;
                 string name;
                 cin>>age;
                 if(age < 0 || age > 100)
                 {
+                    // Validate age input
                     while(age > 100 || age <= 0)
                     {
                         cout<<"You have entered an invalid age. Please enter a valid age(0-99) here: ";
@@ -61,20 +89,24 @@ int main()
                 }
                 if(age < 18 && age >= 0)
                 {
+                    // Inform user about age restriction
                     cout<<"We do not allow minors to take on loans. Our apologies."<<endl;
                 }
                 else
                 {
+                    // Get name of new loan holder
                     cin.ignore();
                     cout<<"Please enter loan holder's name here: ";
                     getline(cin, name);
                 }
+                // Create new Person object and add to vector
                 Person person(name, age);
                 people.push_back(person);
                 break;
             }
             case 2:
             {
+                // Creating a new loan
                 cin.ignore();
                 cout<<"Please enter name loan will be stored under: ";
                 string name;
@@ -85,6 +117,7 @@ int main()
                     if(name == person.getName())
                     {
                         validPerson = true;
+                        // Choose loan type
                         cout<<"Please enter type of loan you would like to create.\nPersonal Loan: 1\nCar Loan: 2\nHouse Loan: 3\nBusiness Loan: 4"<<endl;
                         cout<<"Please enter your choice here: ";
                         int loanChoice;
@@ -111,6 +144,7 @@ int main()
                             cin>>termLength;
                         }
 
+                        // Create new loan object based on user choice
                         if(loanChoice == 1)
                         {
                             Loan loan(person, totalAmount, "4/24/24", termLength);
@@ -142,6 +176,7 @@ int main()
             }
             case 3:
             {
+                // Print loan information for a given loan holder
                 cin.ignore();
                 cout<<"Please enter loan holder's name to display loans: ";
                 string name;
@@ -159,7 +194,7 @@ int main()
                             {
                                 loan.print();
                             }
-                            }
+                        }
                     }
                 }
             
@@ -171,11 +206,13 @@ int main()
             }
             case 4:
             {
+                // Exit program
                 cout << "Thank you for using our loan calculator." << endl;
                 break;
             }
             default:
             {
+                // Invalid choice
                 cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
                 break;
             }
